103faa3684e1829b93da39d69d9cb696df9eca3e
Gegham Samvelyan <vamosgs2001@gmail.com>
2017-08-03 09:34:55 +0400

    v.3.5

--- src/App.jsx
+++ src/App.jsx
@@ -30,9 +30,8 @@ class App extends Component {
         const apiKey = "8e654d5abe58444d";
         console.log( country_code, city )
         const apiUrl = `https://api.wunderground.com/api/${apiKey}/conditions/q/${country_code}/${city}.json`;
-
         console.log(apiUrl)
-        fetch(apiUrl )
+        fetch(apiUrl)
             .then((response) => response.json())
             .then((data) => {
                 this.setState({
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,28 +1,52 @@
 import React from 'react';
 
-class Search extends React.Component{
-    constructor (props) {
+class Search extends React.Component {
+    constructor(props) {
         super(props);
-        this.state = {newCity: ""}
+        this.state = {
+            newCity: "",
+            send: false,
+            newTemp: "",
+            country_code: ""
+        };
         this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
+
     handleChange(event) {
         this.setState({
             newCity: event.target.value
-        } );
-     console.log(event.target.value)
+        });
+        console.log(event.target.value)
 
+    }
+    handleSubmit(event) {
+        event.preventDefault();
+        const apiUrl = `https://api.wunderground.com/api/8e654d5abe58444d/conditions/q/UK/${this.state.newCity}.json`;
+        console.log(apiUrl);
+        fetch(apiUrl)
+            .then((response) => response.json())
+            .then((data) => {
+                this.setState({
+                    newTemp:  data.current_observation.temp_c,
+                    send: true
+                });
+                console.log(this.state)
+            })
     }
 
     render() {
+        const {send, newCity,   newTemp} = this.state;
         return (
-            <div className="Search" >
-                <form>
-                    <input onChange={this.handleChange}
-                           value={this.state.newCity}
-                           type="text"
-                           placeholder="Search for other City"
-                           noValidate/>
+            <div className="Search">
+                <p>{newCity} {newTemp}<span>&deg;c</span></p>
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        onChange={this.handleChange}
+                        value={newCity}
+                        type="text"
+                        placeholder="Search for other City"
+                        noValidate/>
                 </form>
             </div>
         )
